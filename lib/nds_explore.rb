fc6a7d0620b6aaf79015dd7056e070a40e1d6417
Reese M Bowman <reesemb1@gmail.com>
2019-12-23 05:30:50 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,13 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  movie_index = 0 
+  while movie_index < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][movie_index][:title]
+    movie_index += 1
+  end
 end
